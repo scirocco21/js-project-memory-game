2f2ac503ca3ee1c6c79d331530bcb1ce1bf44f5a
Sebastian <sebgertz@gmail.com>
2018-09-28 16:21:19 -0400

    final commit before submission

--- js/app.js
+++ js/app.js
@@ -5,10 +5,10 @@ let openCards = [];
 
 // set starting values for each game
 function initializeValues() {
-  matches = []
+  matches = [];
   scores = 3;
   moves = 0;
-  moveCounter.textContent = moves
+  moveCounter.textContent = moves;
   document.getElementById("sec").innerHTML = 0;
   document.querySelector("ul.stars").innerHTML = stars;
 }
@@ -17,13 +17,13 @@ function initializeValues() {
 function setGame() {
   let cardList = document.querySelectorAll("li.card");
   let shuffledCards = shuffle(Array.from(cardList));
-  document.querySelector("ul.deck").innerHTML = ""
+  document.querySelector("ul.deck").innerHTML = "";
   for (let i = 0; i < shuffledCards.length; i++) {
     document.querySelector("ul.deck").appendChild(shuffledCards[i]);
     cardList[i].className = "card";
     cardList[i].addEventListener("click", clickHandler);
   }
-  initializeValues()
+  initializeValues();
 }
 
 function shuffle(array) {
@@ -40,57 +40,57 @@ function shuffle(array) {
 
 // helper functions for the main game loop
 function openCard(card) {
-  openCards.push(card)
+  openCards.push(card);
   card.classList.add("open", "show");
   card.removeEventListener("click", clickHandler);
 }
 
 function removeCards() {
-  openCards = []
+  openCards = [];
 }
 
 // determine whether two cards are a match
 function isMatch(card1, card2) {
-  return card1.querySelector("i").className === card2.querySelector("i").className ? true : false
+  return card1.querySelector("i").className === card2.querySelector("i").className ? true : false;
 }
 
 // if so, change their styling and remove event listener
 function handleMatch(card1, card2) {
-  card1.classList.add("match")
-  card2.classList.add("match")
-  card1.removeEventListener("click", clickHandler)
-  card2.removeEventListener("click", clickHandler)
-  matches.push(card1, card2)
+  card1.classList.add("match");
+  card2.classList.add("match");
+  card1.removeEventListener("click", clickHandler);
+  card2.removeEventListener("click", clickHandler);
+  matches.push(card1, card2);
 }
 
 // otherwise flip them after a short interval and restore listeners
 function handleMismatch(card1, card2) {
-  setTimeout(function() {card1.classList.remove("open", "show")}, 1000)
-  setTimeout(function() {card2.classList.remove("open", "show")}, 1000)
-  card1.addEventListener("click", clickHandler)
-  card2.addEventListener("click", clickHandler)
+  setTimeout(function() {card1.classList.remove("open", "show")}, 1000);
+  setTimeout(function() {card2.classList.remove("open", "show")}, 1000);
+  card1.addEventListener("click", clickHandler);
+  card2.addEventListener("click", clickHandler);
 }
 
 // helper function that updates star display based on moves score
 function updateStars() {
-  let starList = document.querySelector("ul.stars")
+  let starList = document.querySelector("ul.stars");
   starList.removeChild(starList.children[0]);
-  let node = document.createElement("li")
+  let node = document.createElement("li");
   node.className = "fa fa-star-o";
 }
 
 function updateMoves() {
   moves += 1;
-  moveCounter.textContent = moves
+  moveCounter.textContent = moves;
 }
 
 function updateScore() {
   if (scores === 3 && moves > 12) {
-    scores -= 1
-    displayStars()
+    scores -= 1;
+    displayStars();
   } else if (scores === 2 && moves > 16) {
-    scores -= 1
-    updateStars()
+    scores -= 1;
+    updateStars();
   }
 }
 
@@ -101,7 +101,7 @@ function gameOver() {
 // main game play
 function clickHandler() {
   if (moves === 0) {
-    start()
+    start();
   }
   if (openCards.length === 0) {
     openCard(this);
@@ -109,19 +109,20 @@ function clickHandler() {
     openCard(this);
     let previousCard = openCards[0];
     if (isMatch(this, previousCard)) {
-      handleMatch(this, previousCard)
+      handleMatch(this, previousCard);
     } else {
-      handleMismatch(this, previousCard)
+      handleMismatch(this, previousCard);
     }
     updateMoves();
     updateScore();
-    setTimeout(removeCards, 650)
+    setTimeout(removeCards, 650);
   }
   if (gameOver()) {
-    stop()
-    setTimeout(openModal, 1000)
+    stop();
+    setTimeout(openModal, 1000);
   }
 }
+
 // button handlers
 let againBtn = document.getElementById("repeat");
 let closeBtn = document.getElementById("closeBtn");
@@ -143,7 +144,7 @@ closeBtn.addEventListener('click', closeModal);
 function openModal() {
   let summary = document.getElementById("summary");
   let seconds = document.getElementById("sec");
-  let scores = document.querySelectorAll("ul.stars li i")
+  let scores = document.querySelectorAll("ul.stars li i");
   summary.innerHTML = `Your score was ${scores.length} and you took ${seconds.textContent} seconds`;
   modal.style.display = "block";
 }
@@ -152,13 +153,14 @@ function closeModal() {
   modal.style.display = "none";
 }
 
+// lets players exit modal by clicking on darkened surface
 function clickOutside(e) {
   if (e.target == modal) {
-    modal.style.display = "none"
+    modal.style.display = "none";
   }
 }
 
-window.addEventListener("click", clickOutside)
+window.addEventListener("click", clickOutside);
 
 // game timer
 let timerInterval = null;
@@ -171,7 +173,7 @@ function start() {
 }
 
 function changeValue() {
-  value = parseFloat(value)
+  value = parseFloat(value);
   document.getElementById("sec").innerHTML = ++value;
 }
 
